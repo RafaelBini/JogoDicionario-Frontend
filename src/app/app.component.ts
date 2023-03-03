814b0a9f9ce245efd12bbd78f36ce17c9af8ed28
Rafael Bini <rfabini1996@gmail.com>
2023-03-03 18:17:12 -0300

    Melhorado o podium

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { ApiService } from './services/api.service';
 import { Component } from '@angular/core';
 import { FireService } from './services/fire.service';
@@ -8,11 +9,24 @@ import { FireService } from './services/fire.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isLoading = false;
 
   constructor(
     fireService: FireService,
-    apiService: ApiService
+    apiService: ApiService,
+    private router: Router
   ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        event.url
+        console.log('this.isLoading = true; ')
+        this.isLoading = true;
+      }
+      else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+        console.log('this.isLoading = false; ')
+        this.isLoading = false;
+      }
+    })
     apiService.registerDelta();
   }
 
--- src/app/dialogs/podium-dialog/podium-dialog.component.ts
+++ src/app/dialogs/podium-dialog/podium-dialog.component.ts
@@ -13,9 +13,43 @@ export class PodiumDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public room: Room
   ) { }
 
+  podiumDetails = [
+    { maxHeight: 10, height: '5px' },
+    { maxHeight: 10, height: '5px' }
+  ]
 
 
   ngOnInit(): void {
+    // Calc heights
+    const maxHeight = (300 - 62);
+
+
+    this.podiumDetails[0].maxHeight = maxHeight;
+    this.podiumDetails[1].maxHeight = (this.room.users[1].score / this.room.users[0].score) * maxHeight;
+
+    if (this.room.users.length >= 3) {
+      this.podiumDetails.push({ maxHeight: 10, height: '5px' });
+      this.podiumDetails[2].maxHeight = (this.room.users[2].score / this.room.users[0].score) * maxHeight;
+    }
+
+
+    var currHeight = 5;
+    const interv = setInterval(() => {
+
+      for (let podium of this.podiumDetails) {
+        if (currHeight <= podium.maxHeight) {
+          podium.height = `${currHeight}px`
+        }
+      }
+
+      if (currHeight >= maxHeight) {
+        clearInterval(interv);
+      }
+
+      currHeight++;
+
+    }, 7)
+
   }
 
 
